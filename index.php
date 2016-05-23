444e2a504640b2289f07c08039d7abee30d58c68
Arik Liber <arik.liber@startapp.com>
2016-05-23 10:22:15 +0300

    .

--- index.php
+++ index.php
@@ -12,21 +12,8 @@
         echo $_GET['hub_challenge'];
         
     } else {
-        //logMessage('Wrong verification token', 'error');
-        //echo 'Error, wrong validation token';
-        
-        //file_put_contents('php://stderr', http_build_query($_POST)."\n", FILE_APPEND);
-        //echo print_r(http_build_query($_POST));
-        
-        //logMessage(http_build_query($_POST),"success");
-        
+
         $data = json_decode(file_get_contents('php://input'), true);
-        
-        
-          
-       // logMessage(http_build_query($_POST),"success");
-        
-        //logMessage(print_r($data, true), "data");
         
         $messaging_events = $data['entry'][0]['messaging'];
         
@@ -46,7 +33,7 @@
                 //Send response
                 $token = 'EAAOZCEaUNtxIBAL1bafIm0IYzlo8YcLxjxklu7hjyi8QC4yzwFXFfK0p8qDluJxmGP3lE1roZAOwvCBr7F8dqx0Of7kpTgGRZBncZC1gxtHxlT79Lpbgg4LIfAZA7ZAVVgsskxPd3RbJjsSRcX5rJYRnd8sQbjW4LMBMbgICIf5QZDZD';
                 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$token;
-                $postData = json_encode(array('recipient' => array('id' => $sender['id']), 'message' => array('text' => 'GOT IT!!!' )));
+                $postData = json_encode(array('recipient' => array('id' => $sender['id']), 'message' => array('text' => 'Did you say '.$text.'?' )));
                  
                 logMessage($postData, "data-sent");
                  
@@ -66,32 +53,6 @@
                 } else {
                     logMessage(print_r($result, true), "success");
                 }
-
-                /*
-                var token = "<page_access_token>";
-
-                function sendTextMessage(sender, text) {
-                    messageData = {
-                        text:text
-                    }
-                request({
-                        url: 'https://graph.facebook.com/v2.6/me/messages',
-                        qs: {access_token:token},
-                        method: 'POST',
-                        json: {
-                        recipient: {id:sender},
-                        message: messageData,
-                        }
-                    }, function(error, response, body) {
-                        if (error) {
-                        console.log('Error sending message: ', error);
-                        } else if (response.body.error) {
-                        console.log('Error: ', response.body.error);
-                        }
-                    });
-                }
-                */
-                
                 
             } else {
                 logMessage(print_r($event, true), "error");
