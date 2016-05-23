3e8c0c428a3ecd5956c4dc1a281c4c43211a5374
Arik Liber <arik.liber@startapp.com>
2016-05-23 10:06:59 +0300

    .

--- index.php
+++ index.php
@@ -47,7 +47,9 @@
                 $token = 'EAAOZCEaUNtxIBAL1bafIm0IYzlo8YcLxjxklu7hjyi8QC4yzwFXFfK0p8qDluJxmGP3lE1roZAOwvCBr7F8dqx0Of7kpTgGRZBncZC1gxtHxlT79Lpbgg4LIfAZA7ZAVVgsskxPd3RbJjsSRcX5rJYRnd8sQbjW4LMBMbgICIf5QZDZD';
                 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$token;
                 $postData = json_encode(array('recipient' => array('id' => $sender['id']), 'message' => 'GOT IT!!!'));
-
+                 
+                logMessage($postData, "data-sent");
+                 
                 $options = array(
                     'http' => array(
                         'header'  => "Content-type: application/json\r\n"
