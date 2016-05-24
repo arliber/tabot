83444c654dcb11cb30f84b430cf371b3aee3fcd7
Arik Liber <arik.liber@startapp.com>
2016-05-24 19:18:29 +0300

    Work on header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 
+var ctx = document.getElementById("myChart");
+
+
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+            
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+            gridLines: {
+                display: false
+            }
+        }
+       
+    }
+});
